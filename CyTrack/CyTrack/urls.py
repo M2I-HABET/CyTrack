d7b630830626b4a2ae6c53c343a55971e7b80a62
Matthew Plewa <matthewplewa@gmail.com>
2018-12-18 22:59:41 -0600

    Adding Cesium to prediction along with fixing some html

--- CyTrack/CyTrack/urls.py
+++ CyTrack/CyTrack/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^predict/', include('CyPredict.urls')),
     url(r'^$', include('CyHome.urls')),
+
 ]
