74145dfd0dc42623a76ac2aec91818d99ccd12dc
Matthew Plewa <matthewplewa@gmail.com>
2019-01-29 21:07:52 -0600

    Prediction now fully works. Added form to fill in all of the perameters.

--- CyTrack/CyPredict/views.py
+++ CyTrack/CyPredict/views.py
@@ -1,30 +1,36 @@
 from django.shortcuts import render
 from CyPredict.predictor.Predictor.LatexPrediction import LatexHAB
 from django.contrib.auth.decorators import login_required
+import xml.etree.ElementTree as ET
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 @login_required
 def predictionOutPut(request):
-    year=request.POST.get("year", '')
-    print("Checking")
-    print("year"+str(year)) 
-    #month
-    #day
-    #hour
-    #minute
-    #lat
-    #lon
-    #altitude
-    #mass
-    #lift
-    #pArea
-    #pcd
-    #bmass
+    numkeys = 0
+    for key in request.POST:
+        numkeys = numkeys + 1
+    print(numkeys)
+    if numkeys != 14:
+        return HttpResponseRedirect('../')
+    year = int(request.POST.get("year"))
+    month = request.POST.get("month")
+    day = int(request.POST.get("day"))
+    hour = int(request.POST.get("hour"))
+    minute = int(request.POST.get("min"))
+    lat = float(request.POST.get("lat"))
+    lon = float(request.POST.get("lon"))
+    altitude = int(request.POST.get("altitude"))
+    mass = float(request.POST.get("mass"))
+    lift = float(request.POST.get("lift"))
+    pArea = float(request.POST.get("pArea"))
+    pcd = float(request.POST.get("pcd"))
+    bmass = float(request.POST.get("bmass"))
     tStep = 15
     pre = LatexHAB(tStep)
+    # 2019, "Jan", 31, 18, 0, 42.0308, -93.6319, -0,3.0, 4.667, 2.0, 1.5, 2000
     # year,month,day,hour,minute,lat,lon,altitude,mass,lift,pArea,pcd,bmass
-    pre.setValues(2019, "Jan", 31, 18, 0, 42.0308, -93.6319, -0,
-                  3.0, 4.667, 2.0, 1.5, 2000)
+    pre.setValues(year,month,day,hour,minute,lat,lon,altitude,mass,lift,pArea,pcd,bmass)
     results = pre.runPrediction()
     del pre
     print(results[0])
---
+++ helper Files/generateops.py
@@ -0,0 +1,5 @@
+
+nums = list(range(0,60))
+
+for num in nums:
+    print("<option value=\""+str(num)+"\">"+str(num)+"</option>")
